9c76c64a5f9cbbf833811cc456f242c2ef87d38c
heraldofortuna <heraldofortuna@gmail.com>
2021-06-15 12:40:18 -0500

    Counter project was completed

--- scripts/counter.js
+++ scripts/counter.js
@@ -3,3 +3,17 @@ let count = 0;
 // Select value and buttons;
 const value = document.getElementById("value");
 const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
